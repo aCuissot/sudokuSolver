18637c582f4caefbed09567fed56ed7942070e94
aCuissot <harrypotter9752@gmail.com>
2021-05-17 20:36:00 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, on peut trouver la solution mais c'est pas ça qui est retourné...

--- solvers/brutForceSolver.py
+++ solvers/brutForceSolver.py
@@ -7,13 +7,12 @@ from utils.extractDataFromGrid import *
 def checkIfPartialGridGotError(sudoku, numberOfRaws):
     """
 
+    :param numberOfRaws: the number of raws completed ad so to check
     :param sudoku: the partial sudoku string, it need to be a complete number of raws
     :return: True if the grid don't have trivial error
              False if it have at least one
     """
-    sudoku = sudoku[:numberOfRaws*constantes.SIZEOFALINE]
     # check each raw
-    numberOfRaws = int(len(sudoku) / constantes.SIZEOFALINE)
     for i in range(numberOfRaws):
         currRaw = getRaw(sudoku, i)
         for j in range(1, constantes.SIZEOFALINE + 1):
@@ -21,28 +20,43 @@ def checkIfPartialGridGotError(sudoku, numberOfRaws):
                 return False
     # check each partial column
     for i in range(constantes.SIZEOFALINE):
-        currPartialColumn = getColumnPartial(sudoku, i)
+        currColumn = getColumn(sudoku, i)
         for j in range(1, constantes.SIZEOFALINE + 1):
-            if currPartialColumn.count(str(j)) > 1:
+            if currColumn.count(str(j)) > 1:
+                return False
+    return True
+
+
+def checkMoreSmart(sudoku):
+    """
+    here we check only columns because with the way we fill raws, we cannot have error in a raw
+    :param sudoku: the sudoku string
+    :return: True if the grid don't have trivial error
+             False if it have at least one
+    """
+    for i in range(constantes.SIZEOFALINE):
+        currColumn = getColumn(sudoku, i)
+        for j in range(1, constantes.SIZEOFALINE + 1):
+            if currColumn.count(str(j)) > 1:
                 return False
     return True
 
 
 def solveWithBrutForce(sudoku, i=0):
     # case the previous change was an error
-    if i != 0 and not checkIfPartialGridGotError(sudoku, i):
+    if i != 0 and not checkMoreSmart(sudoku):
         return
-
     # case we don't have errors (see previous if loop) and the grid is full => we have the right solution
     if i == constantes.SIZEOFALINE:
+        print("solution:" + sudoku)
         return sudoku
 
     currRaw = getRaw(sudoku, i)
     missingNumbers = missingNumbersInEntity(currRaw)
     for permutation in permutations(missingNumbers):
-        print('i=' + str(i))
-        print('permutation:' + ','.join(permutation))
-        print(sudoku)
+        #print('i=' + str(i))
+        #print('permutation:' + ','.join(permutation))
+        #print(sudoku)
         solveWithBrutForce(setPermutation(sudoku, i, permutation), i+1)
 
 
@@ -51,3 +65,4 @@ solution = "86437125932584976197126584343619258719865743225748391668973412571352
 
 solved = solveWithBrutForce(sudoKu)
 print(solved)
+
