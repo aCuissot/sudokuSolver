0113ecd458638b8629c5f342cab2e3edf06fa996
aCuissot <harrypotter9752@gmail.com>
2021-05-17 20:59:02 +0200

    aled

--- solvers/brutForceSolver.py
+++ solvers/brutForceSolver.py
@@ -2,6 +2,7 @@ from itertools import permutations
 from utils.gettersAndSetters import *
 import constantes
 from utils.extractDataFromGrid import *
+from utils.formatAndDisplay import displaySudoku
 
 
 def checkIfPartialGridGotError(sudoku, numberOfRaws):
@@ -27,7 +28,7 @@ def checkIfPartialGridGotError(sudoku, numberOfRaws):
     return True
 
 
-def checkMoreSmart(sudoku):
+def checkIfPartialGridColumnsGotError(sudoku):
     """
     here we check only columns because with the way we fill raws, we cannot have error in a raw
     :param sudoku: the sudoku string
@@ -42,27 +43,44 @@ def checkMoreSmart(sudoku):
     return True
 
 
+# this function seems check to many options... more than expected - > check it
 def solveWithBrutForce(sudoku, i=0):
-    # case the previous change was an error
-    if i != 0 and not checkMoreSmart(sudoku):
-        return
-    # case we don't have errors (see previous if loop) and the grid is full => we have the right solution
-    if i == constantes.SIZEOFALINE:
-        print("solution:" + sudoku)
-        return sudoku
+    """
+    reccursively solve sudoku
+    :param sudoku: sudoku string
+    :param i: the current raw trying to
+    :return: the solved sudoku
+    """
+    # case the previous change was not an error
+    if not (i != 0 and not checkIfPartialGridColumnsGotError(sudoku)):
+        # case we don't have errors (see previous if loop) and the grid is full => we have the right solution
+        if i == constantes.SIZEOFALINE:
+            print("solution:" + sudoku)
+            return sudoku
 
-    currRaw = getRaw(sudoku, i)
-    missingNumbers = missingNumbersInEntity(currRaw)
-    for permutation in permutations(missingNumbers):
-        #print('i=' + str(i))
-        #print('permutation:' + ','.join(permutation))
-        #print(sudoku)
-        solveWithBrutForce(setPermutation(sudoku, i, permutation), i+1)
+        currRaw = getRaw(sudoku, i)
+        missingNumbers = missingNumbersInEntity(currRaw)
+        for permutation in permutations(missingNumbers):
+            #print('i=' + str(i))
+            #print('permutation:' + ','.join(permutation))
+            #print(sudoku)
+            solveWithBrutForce(setPermutation(sudoku, i, permutation), i+1)
+    return
 
 
 sudoKu = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
 solution = "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
 
-solved = solveWithBrutForce(sudoKu)
-print(solved)
+
+a1="864371259325849761971265843436192587198657432257483916689734125713528694542916378"
+a2="864375219325749861279861543436152987198637452951483726687294135713528694542916378"
+print(checkIfPartialGridColumnsGotError(a1))
+print(checkIfPartialGridColumnsGotError(a2))
+displaySudoku(sudoKu)
+displaySudoku(a1)
+displaySudoku(a2)
+displaySudoku(solution)
+print('samere pourquoi y a plusieurs solutions?AAAAAAAaaaaaaaaaaaaaaaaaaa')
+
+print(solveWithBrutForce(sudoKu))
 
