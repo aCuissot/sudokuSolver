82aa5cad3ea5dd8827481031fd2aed5850766bf3
aCuissot <harrypotter9752@gmail.com>
2021-05-18 18:27:44 +0200

    adding Readme.md

---
+++ Readme.md
@@ -0,0 +1,9 @@
+This project is a personal project which have for goal to:
+
+1. Solve a sudoku grid with different ways (brut force method, analytic method and using neural networks) to compare them (time performance, ram consumption, ...)
+
+2. Detect and solve sudoku grid in a given picture, with different parameters like the used method, the input format (image path, url, webcam capture, sudoku string (maybe with an interface to enter it easily)), the output format (write result on the image, display it in the standard output ...)
+
+3. Generate solvable grids (grid with only one solution)
+ 
+The sudokus here are only square sudokus the size can be configured in `constante.py`.
--- detection/LineDetector.py
+++ detection/LineDetector.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
+
+"""
 img = cv.imread('../data/sudoku.png', 0)
 laplacian = cv.Laplacian(img, cv.CV_64F)
 sobelx = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
@@ -10,6 +12,35 @@ plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(2, 2, 2), plt.imshow(laplacian, cmap='gray')
 plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 3), plt.imshow(sobelx, cmap='gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 4), plt.imshow(sobely, cmap='gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+plt.show()"""
+
+
+def avg(a):
+    s = 0
+    for i in a:
+        s+=i
+    return s/len(a)
+
+img = cv.imread('../data/sudoku.png', 0)
+laplacian = cv.Laplacian(img, cv.CV_64F)
+sobelx = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
+sobely = cv.Sobel(img, cv.CV_64F, 0, 1, ksize=5)
+sobelSum = sobelx + sobely
+"""
+for j in sobelSum:
+    print('min: ' + str(min(j)) + ' - max: ' + str(max(j)) + ' - avg: ' + str(avg(j)))
+"""
+
+ret, th1 = cv.threshold(sobelSum, -1000, 10000, cv.THRESH_BINARY)
+
+plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 2), plt.imshow(th1, cmap='gray')
+plt.title('Sobel Sum + threshold'), plt.xticks([]), plt.yticks([])
 plt.subplot(2, 2, 3), plt.imshow(sobelx, cmap='gray')
 plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
 plt.subplot(2, 2, 4), plt.imshow(sobely, cmap='gray')
